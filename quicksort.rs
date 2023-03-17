4d3d8446e4def9ea44fbf49be2c186ff130294fc
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-03-17 13:38:00 +0600

    feat: optimized quick sort

--- quicksort.rs
+++ quicksort.rs
@@ -3,24 +3,54 @@ fn quicksort(arr: &mut [i32]) {
     if len < 2 {
         return;
     }
-    let pivot = partition(arr);
-    quicksort(&mut arr[..pivot]);
-    quicksort(&mut arr[pivot + 1..]);
+    if len < 10 {
+        insertion_sort(arr);
+        return;
+    }
+    let pivot = median_of_three(arr);
+    let (mut left, mut right) = (0, len - 1);
+    loop {
+        while arr[left] < pivot {
+            left += 1;
+        }
+        while arr[right] > pivot {
+            right -= 1;
+        }
+        if left >= right {
+            break;
+        }
+        arr.swap(left, right);
+        left += 1;
+        right -= 1;
+    }
+    quicksort(&mut arr[..right + 1]);
+    quicksort(&mut arr[right + 1..]);
 }
 
-fn partition(arr: &mut [i32]) -> usize {
-    let len = arr.len();
-    let pivot_index = len / 2;
-    arr.swap(pivot_index, len - 1);
-    let mut i = 0;
-    for j in 0..len - 1 {
-        if arr[j] < arr[len - 1] {
-            arr.swap(i, j);
-            i += 1;
+fn insertion_sort(arr: &mut [i32]) {
+    for i in 1..arr.len() {
+        let mut j = i;
+        while j > 0 && arr[j - 1] > arr[j] {
+            arr.swap(j - 1, j);
+            j -= 1;
         }
     }
-    arr.swap(i, len - 1);
-    i
+}
+
+fn median_of_three(arr: &mut [i32]) -> i32 {
+    let len = arr.len();
+    let mid = len / 2;
+    let last = len - 1;
+    if arr[0] > arr[mid] {
+        arr.swap(0, mid);
+    }
+    if arr[mid] > arr[last] {
+        arr.swap(mid, last);
+        if arr[0] > arr[mid] {
+            arr.swap(0, mid);
+        }
+    }
+    arr[mid]
 }
 
 fn main() {
